ded1c785daf9490b70a389d94b7e5c2a4f023484
Kyle Cordes <kyle.cordes@oasisdigital.com>
2022-04-11 18:29:41 -0500

    curriculum update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,9 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
     HttpClientModule
   ],
   bootstrap: [AppComponent]
